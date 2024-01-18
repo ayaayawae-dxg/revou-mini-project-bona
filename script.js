99dd18db5e39a64218b4971fd624e7128a2a68d7
Bonaventura Sanjaya <bonaventura.sanjaya@dexagroup.com>
2024-01-18 10:45:01 +0700

    add functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+const init = () => {
+    console.log('Loaded')
+}
+
+document
+    .getElementById('participant-form')
+    .addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const participantTable = document.getElementById('participant-data')
+
+        const nama = e.target['nama'].value
+        const umur = e.target['umur'].value
+
+        const newTr = document.createElement('tr')
+        const tdNama = document.createElement('td')
+        const tdUmur = document.createElement('td')
+        tdNama.innerHTML = nama
+        tdUmur.innerHTML = umur
+
+        newTr.appendChild(tdNama)
+        newTr.appendChild(tdUmur)
+
+        participantTable.appendChild(newTr)
+    })
